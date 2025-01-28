1734ad151c09c51a72f16e5269c2afffa99dfa61
Russilx <sanmartinesteban123@gmail.com>
2025-01-28 02:54:19 -0300

    1

--- plugins/fun-calculador.js
+++ plugins/fun-calculador.js
@@ -32,7 +32,7 @@ const handler = async (m, { conn, command, text, usedPrefix }) => {
       break;
     case 'pajero':
     case 'pajera':
-      emoji = 'ADICTO A LA PAJA';
+      emoji = '😏💦';
       if (percentages < 50) {
         description = `${tradutor.texto7[0]} ${text.toUpperCase()} ${tradutor.texto7[1]} ${percentages}% ${command}. ${emoji}*\n${tradutor.texto7[2]}`;
       } else if (percentages > 100) {
