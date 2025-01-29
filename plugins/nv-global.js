126d44b62cbf54fa0b414bddaa6c1c008b2c110b
Russilx <sanmartinesteban123@gmail.com>
2025-01-29 02:35:08 -0300

    1

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -286,7 +286,7 @@ handler.all = async function(m, {conn}) {
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
 
-  if (!chat.isBanned && m.text.match(/(pvp)/gi)) {
+  if (!chat.isBanned && m.text.match(/(querespvp)/gi)) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
     const vn = './src/assets/audio/pvp.mp3';
